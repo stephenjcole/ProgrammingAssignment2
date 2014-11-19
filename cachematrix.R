0a45846fb4cb87e21bea90b6cf47c68b42e090d6
Steve Cole <scole@usa.net>
2014-11-18 21:42:11 -0500

    Starter work on makeCacheMatrix
    Renaming from the “makemean” sample in the assignment. Not even close
    to complete!

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        stored_inv <- NULL
+        set <- function(y) {
+                x <<- y
+                stored_inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(matrix_inv) stored_inv <<- matrix_inv
+        getinv <- function() stored_inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
